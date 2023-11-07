ae87b81e6b62fb780290618fbdf00d0c79a348fc
Xin Yu <chuaxc@unimelb.edu.au>
2023-11-08 03:40:56 +1100

    Migrated markdoc components to mdx components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Badge } from "./Badge.astro";
+export { default as Subject } from "./Subject.astro";
+
+export { default as Audio } from "./markdown/Audio.astro";
+export { default as CodeBlock } from "./markdown/CodeBlock.astro";
+export { default as CodePenEmbed } from "./markdown/CodePenEmbed.astro";
+export { default as CodeSandboxEmbed } from "./markdown/CodeSandboxEmbed.astro";
+export { default as GitHubGistEmbed } from "./markdown/GitHubGistEmbed.astro";
+export { default as Mermaid } from "./markdown/Mermaid.astro";
+export { default as PyCode } from "./markdown/PythonCode.astro";
+export { default as YouTubeEmbed } from "./markdown/YouTubeEmbed.astro";
---
+++ src/components/markdown/index.js
@@ -0,0 +1,8 @@
+export { default as Audio } from "./Audio.astro";
+export { default as CodeBlock } from "./CodeBlock.astro";
+export { default as CodePenEmbed } from "./CodePenEmbed.astro";
+export { default as CodeSandboxEmbed } from "./CodeSandboxEmbed.astro";
+export { default as GitHubGistEmbed } from "./GitHubGistEmbed.astro";
+export { default as Mermaid } from "./Mermaid.astro";
+export { default as PyCode } from "./PythonCode.astro";
+export { default as YouTubeEmbed } from "./YouTubeEmbed.astro";
