39d6b62ed073ce465b06a09098cd271cfba1a929
Xin Yu <chuaxc@unimelb.edu.au>
2023-11-07 23:39:03 +1100

    Deployment Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://chuahxinyu.github.io",
+  base: "/docs",
   integrations: [
     starlight({
       title: "Xin Yu's Docs",
